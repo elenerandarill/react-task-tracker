5005642c2292cd5ad313096957ef49b255c9e797
elener <paulina.wojno@gmail.com>
2021-07-04 21:53:38 +0200

    Using mockup DB from json-server. Fetching data.

--- src/App.js
+++ src/App.js
@@ -1,15 +1,29 @@
 import Header from "./components/Header";
 import Tasks from "./components/Tasks";
 import AddTask from "./components/AddTask";
-import taskList from "./backend/taskList";
-import { useState } from "react";
+import {useEffect, useState} from "react";
 
 
 function App() {
-    const [tasks, setTasks] = useState(taskList)
+    const [tasks, setTasks] = useState([])
     const [showAddTask, setShowAddTask] = useState(false)
 
-    // AddTask
+    useEffect(() => {
+        const getTasks = async () => {
+            const tasksFromServer = await fetchTasks()
+            setTasks(tasksFromServer)
+        }
+
+        getTasks()
+    }, [])
+
+    // Fetch Tasks
+    const fetchTasks = async () => {
+        const res = await fetch("http://localhost:5000/tasks")
+        return await res.json()
+    }
+
+    // Add Task
     const addTask = (task) => {
         const id = Math.floor(Math.random() * 1000) + 1
         const newTask = {...task, id}
